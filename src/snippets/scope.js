b35b58aac3da96eb0ae87348ce955f99f435b5b4
Gorrie Coe <gorriecoe@gmail.com>
2020-07-10 14:36:04 +1200

    Classified sanchez

---
+++ src/snippets/scope.js
@@ -0,0 +1,8 @@
+const { arrays, scope } = require('../utilities')
+
+module.exports  = (requiredScope, compareScope) => {
+  return arrays.matchOne(
+    scope.format(requiredScope),
+    scope.format(compareScope)
+  )
+}
