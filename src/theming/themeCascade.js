864e58e2f3aec474b9ef390fdef3af9ca1b11fc1
Gorrie Coe <gorriecoe@gmail.com>
2020-08-13 18:36:44 +1200

    Added thorough check for SilverStripe\View\SSViewer themes

--- src/theming/themeCascade.js
+++ src/theming/themeCascade.js
@@ -1,7 +1,11 @@
 const isset = require('isset')
 
 module.exports = (config) => {
-  if (isset(config)) {
+  if (
+    isset(config) &&
+    isset(config['SilverStripe\\View\\SSViewer']),
+    isset(config['SilverStripe\\View\\SSViewer'].themes)
+  ) {
     return config['SilverStripe\\View\\SSViewer'].themes.filter(themeName => {
       // Remove any variable theme
       return !themeName.startsWith('$')
